6b4cabb140ed78288c2791ce6da68b2660e850e4
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-26 15:07:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    paddle.change(LedSpriteProperty.X, 0 - 1)
+})
+input.onButtonPressed(Button.B, function () {
+    paddle.change(LedSpriteProperty.X, 1)
+})
+let paddle: game.LedSprite = null
 let ball = game.createSprite(2, 1)
-let paddle = game.createSprite(2, 3)
+paddle = game.createSprite(2, 3)
+if (Math.randomBoolean()) {
+    ball.turn(Direction.Left, 45)
+} else {
+    ball.turn(Direction.Right, 45)
+}
 basic.forever(function () {
     ball.move(1)
     ball.ifOnEdgeBounce()
