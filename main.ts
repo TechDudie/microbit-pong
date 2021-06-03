2bd58ec8abbf2c0ab5c9a26ca85010a6bc6a4331
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-06-03 18:29:41 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,18 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     paddle.change(LedSpriteProperty.X, -1)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     paddle.change(LedSpriteProperty.X, 1)
 })
-let paddle : game.LedSprite = null
-let ball = game.createSprite(2, 1)
+let paddle: game.LedSprite = null
+let ball = game.createSprite(randint(0, 4), 1)
 paddle = game.createSprite(2, 3)
 if (Math.randomBoolean()) {
     ball.turn(Direction.Left, 45)
 } else {
     ball.turn(Direction.Right, 45)
 }
-
-basic.forever(function on_forever() {
+basic.forever(function () {
     ball.move(1)
     ball.ifOnEdgeBounce()
     if (ball.isTouching(paddle)) {
@@ -23,14 +22,11 @@ basic.forever(function on_forever() {
         } else {
             ball.turn(Direction.Left, 90)
         }
-        
     }
-    
     if (ball.get(LedSpriteProperty.Y) == 4) {
         game.pause()
         basic.pause(1000)
         basic.showString("GAME OVER    SCORE: " + ("" + game.score()))
     }
-    
     basic.pause(1000)
 })
